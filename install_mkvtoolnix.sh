cbc617ade69dfce642e6370459ad4432d8d15de4
Iván Masías <ivan.masias.ortiz@gmail.com>
2024-04-27 10:46:51 -0400

    [ADD] isntall MKVToolNix

---
+++ install_mkvtoolnix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+function update(){
+	apt-get update -y
+	apt-get upgrade -y
+	apt-get autoremove -y
+}
+
+source_list=/etc/apt/sources.list.d
+gpgkey_path=/etc/apt/trusted.gpg.d
+
+update > /dev/null
+if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
+function install_mkvtoolnix(){
+	echo "---> Creando APT Source  ... "
+	text="deb ${archtype} https://mkvtoolnix.download/ubuntu/ jammy main"
+	echo $text >>$source_list/mkvtoolnix.download.list
+	echo "---> MKVToolNix Key ... "
+	wget -qO- https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg | gpg --dearmour > $gpgkey_path/gpg-pub-moritzbunkus.gpg
+	echo "---> Actualizando ... "
+	update > /dev/null
+	echo "---> Instalando Paquetes ... "
+	apt-get install apt-transport-https mkvtoolnix mkvtoolnix-gui -y > /dev/null
+	echo ""
+}
+
+echo "Instalando MKVToolNix ..."
+install_mkvtoolnix
+echo "Enjoy 3:)"
