eebdd17d251ff2f16e52db6ba4d43779ccbd2c15
Iván Masías <ivan.masias.ortiz@gmail.com>
2024-10-03 04:20:16 -0300

    [ADD] install node

---
+++ install_node.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+function update(){
+	apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
+}
+
+node_version=18 
+source_list=/etc/apt/sources.list.d
+gpgkey_path=/etc/apt/trusted.gpg.d
+
+update > /dev/null
+if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
+function install_nodejs(){
+    echo "---> Creando APT Source para NodeJs..."
+    nodejs_list="$source_list/nodejs.list"
+    nodejs_gpg="$gpgkey_path/nodejs.gpg"
+
+    if [ ! -f "$nodejs_list" ]; then
+        echo "deb ${archtype} https://deb.nodesource.com/node_$node_version.x nodistro main" | tee "$nodejs_list" > /dev/null
+    else
+        echo "---> El repositorio de NodeJs ya está configurado."
+    fi
+
+    if [ ! -f "$nodejs_gpg" ]; then
+        echo "---> Descargando la clave GPG de NodeJs..."
+        wget -qO- https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmour | sudo tee "$nodejs_gpg" > /dev/null
+    else
+        echo "---> La clave GPG de NodeJs ya está configurada."
+    fi
+
+    echo "---> Actualizando el índice de paquetes..."
+    update > /dev/null
+    echo "---> Instalando NodeJs..."
+    apt-get install apt-transport-https nodejs -y > /dev/null
+    echo "---> NodeJs instalado con éxito."
+}
+
+echo "Instalando NodeJs ..."
+install_nodejs
+echo "Enjoy 3:)"
