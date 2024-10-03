62992f709acd389ed18327700df574cea3c1e632
Iván Masías <ivan.masias.ortiz@gmail.com>
2024-10-03 04:21:47 -0300

    [UPDATE] mejoras para script
    - reconoce si existe llave publica y repositorio previamente instalados

--- install_vscode.sh
+++ install_vscode.sh
@@ -10,16 +10,28 @@ update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
 function install_vscode(){
-	echo "---> Creando APT Source  ... "
-	text="deb ${archtype} https://packages.microsoft.com/repos/code stable main"
-	echo $text >> $source_list/vscode.list
-	echo "---> VScode Key ... "
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmour > $gpgkey_path/microsoft.gpg
-	echo "---> Actualizando ... "
-	update > /dev/null
-	echo "---> Instalando Paquetes ... "
-	apt-get install apt-transport-https code -y > /dev/null
-	echo ""
+    echo "---> Creando APT Source para VSCode..."
+    vscode_list="$source_list/vscode.list"
+    vscode_gpg="$gpgkey_path/microsoft.gpg"
+
+    if [ ! -f "$vscode_list" ]; then
+        echo "deb ${archtype} https://packages.microsoft.com/repos/code stable main" | sudo tee "$vscode_list" > /dev/null
+    else
+        echo "---> El repositorio de VSCode ya está configurado."
+    fi
+
+    if [ ! -f "$vscode_gpg" ]; then
+        echo "---> Descargando la clave GPG de Microsoft..."
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmour | sudo tee "$vscode_gpg" > /dev/null
+    else
+        echo "---> La clave GPG de Microsoft ya está configurada."
+    fi
+
+    echo "---> Actualizando el índice de paquetes..."
+    update > /dev/null
+    echo "---> Instalando VSCode..."
+    apt-get install apt-transport-https code -y > /dev/null
+    echo "---> VSCode instalado con éxito."
 }
 
 echo "Instalando VScode ..."
