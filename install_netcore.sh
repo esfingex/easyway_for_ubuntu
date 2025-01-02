c33a7fc4c008c32ebeaaba5a4f0d18d3f3a44661
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-01-02 19:10:55 -0300

    [UPDATE] mejora en instalación

--- install_netcore.sh
+++ install_netcore.sh
@@ -5,24 +5,39 @@ function update(){
 
 source_list=/etc/apt/sources.list.d
 gpgkey_path=/etc/apt/trusted.gpg.d
-distribution=$(. /etc/os-release;echo $ID/$VERSION_ID)
+version=$(. /etc/os-release;echo $ID/$VERSION_ID)
+distribution=$(. /etc/os-release;echo $VERSION_CODENAME)
 
 update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
 function install_netcore(){
-	echo "---> Creando APT Source  ... "
-	text="deb ${archtype} https://packages.microsoft.com/$distribution/prod jammy main"
-	echo $text >> $source_list/microsoft-prod.list
-	echo "---> VScode Key ... "
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmour > $gpgkey_path/microsoft.gpg
+	echo "---> Creando APT Source para NetCore... "
+	aptlist="$source_list/microsoft-prod.list"
+    aptgpg="$gpgkey_path/microsoft.gpg"
+
+	if [ ! -f "$aptlist" ]; then
+        echo "deb ${archtype} https://packages.microsoft.com/$version/prod $distribution main" | sudo tee "$aptlist" > /dev/null
+    else
+        echo "---> El repositorio ya está configurado."
+    fi
+
+    if [ ! -f "$aptgpg" ]; then
+        echo "---> Descargando la clave GPG de Microsoft..."
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmour | sudo tee "$aptgpg" > /dev/null
+    else
+        echo "---> La clave GPG ya está configurada."
+    fi
+
 	echo "---> Actualizando ... "
 	update > /dev/null
 	echo "---> Instalando Paquetes ... "
 	apt-get install apt-transport-https dotnet-sdk-8.0 -y > /dev/null
 	echo ""
+
 }
 
 echo "Instalando .Net Core ..."
 install_netcore
+
 echo "Enjoy 3:)"
