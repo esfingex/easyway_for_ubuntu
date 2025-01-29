8e97acac7e44d81daa1dec3de9b4ace9fc35eb47
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-01-29 02:18:25 -0300

    [UPDATE] mejoras
    - agrega versión por sistema operativo de manera automatica
    - mejora la forma de ingresar el apt-source y la key de seguridad del server

--- install_nvidia_cuda.sh
+++ install_nvidia_cuda.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Según doc
 #https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#network-repo-installation-for-ubuntu
 
@@ -8,21 +7,29 @@ function update(){
 	apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
 }
 
-ubuntuv='ubuntu2204'
+version=$(. /etc/os-release; echo ${ID}${VERSION_ID//./})
 source_list=/etc/apt/sources.list.d
 gpgkey_path=/etc/apt/trusted.gpg.d
 
+update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
 function nvidia_cuda(){
-    echo "---> Creando APT Source  ... "
-    text="deb https://developer.download.nvidia.com/compute/cuda/repos/${ubuntuv}/${archtype}/ /"
-    echo $text >> $source_list/cuda-${ubuntuv}-${archtype}.list
+    echo "---> Creando APT Source ... "
+	aptlist="$source_list/cuda-${version}-${archtype}.list"
 
-    wget https://developer.download.nvidia.com/compute/cuda/repos/${ubuntuv}/${archtype}/cuda-keyring_1.1-1_all.deb
+    if [ ! -f "$aptlist" ]; then
+        echo "deb ${archtype} https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype}/ /" | sudo tee "$aptlist" > /dev/null
+    else
+        echo "---> El repositorio ya está configurado."
+    fi
+
+    echo "---> Cuda Key ... "
+    wget https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype}/cuda-keyring_1.1-1_all.deb
     dpkg -i cuda-keyring_1.1-1_all.deb
     rm cuda-keyring_1.1-1_all.deb
 
+    #Antigua forma
     #echo "---> Cuda Key ... "
     #wget -qO- https://developer.download.nvidia.com/compute/cuda/repos/${ubuntuv}/${archtype}/cuda-archive-keyring.gpg | gpg --dearmour > $gpgkey_path/cuda-archive-keyring.gpg
     #echo "---> Cuda Priority ... "
