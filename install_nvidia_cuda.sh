7dbcdca034c22bf2e493b9a6cc22921138abd044
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-01-29 12:37:07 -0300

    [UPDATE] mejora en instalación de librerías para cuda
    - ubuntu 2404

--- install_nvidia_cuda.sh
+++ install_nvidia_cuda.sh
@@ -2,10 +2,10 @@
 #Según doc
 #https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#network-repo-installation-for-ubuntu
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Por favor, ejecute como root."
-    exit 1
-fi
+#if [ "$EUID" -ne 0 ]; then
+#    echo "Por favor, ejecute como root."
+#    exit 1
+#fi
 
 #Only Ubuntu
 function update(){
@@ -17,32 +17,33 @@ source_list=/etc/apt/sources.list.d
 gpgkey_path=/etc/apt/trusted.gpg.d
 
 update > /dev/null
-if [ "$(arch)" = "x86_64" ]; then archtype="arch=amd64"; fi
+if [ $(arch) == 'x86_64' ]; then archtype1=[arch=amd64]; fi
+archtype2=x86_64
 
 function nvidia_cuda(){
     echo "---> Creando APT Source ... "
-	aptlist="$source_list/cuda-${version}-${archtype}.list"
+	aptlist="$source_list/cuda-${version}-${archtype2}.list"
 
     if [ ! -f "$aptlist" ]; then
-        echo "deb ${archtype} https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype}/ /" | sudo tee "$aptlist" > /dev/null
+        echo "deb ${archtype1} https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype2}/ /" | sudo tee "$aptlist" > /dev/null
     else
         echo "---> El repositorio ya está configurado."
     fi
-
+        
     echo "---> Cuda Key ... "
-    wget https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype}/cuda-keyring_1.1-1_all.deb
+    wget https://developer.download.nvidia.com/compute/cuda/repos/${version}/${archtype2}/cuda-keyring_1.1-1_all.deb
     if ! dpkg -i cuda-keyring_1.1-1_all.deb; then
         echo "Error al instalar la clave de CUDA."
         exit 1
     fi
-    #rm cuda-keyring_1.1-1_all.deb
+    rm cuda-keyring_1.1-1_all.deb
 
-    #Antigua forma
-    #echo "---> Cuda Key ... "
-    #wget -qO- https://developer.download.nvidia.com/compute/cuda/repos/${ubuntuv}/${archtype}/cuda-archive-keyring.gpg | gpg --dearmour > $gpgkey_path/cuda-archive-keyring.gpg
-    #echo "---> Cuda Priority ... "
-    #wget -qO- https://developer.download.nvidia.com/compute/cuda/repos/${ubuntuv}/${archtype}/cuda-${ubuntuv}.pin 
-    #mv cuda-${ubuntuv}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A4B469963BF863CC
+    apt-key export A4B469963BF863CC | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/nvidia-cuda-keyring.gpg
+    rm /etc/apt/trusted.gpg
+
+    update
+
 }
 
 function install_cuda(){
