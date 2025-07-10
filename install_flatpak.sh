c25af9c6ae6d5644c4fc7a3d7b2e25fe63e4d2a2
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-07-10 01:51:28 -0400

    [ADD] soporte nvidia

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -15,6 +15,92 @@ function install_flatpak(){
 	echo ""
 }
 
+#Beta Install Nvidia with Bottle (Inyect Files)
+function install_flatpak_nvidia(){
+	DEST_DIR="/opt/flatpak-nvidia-libs"
+
+	echo "[+] Creando directorio de Instalación ..."
+	mkdir -p $DEST_DIR
+
+	echo "[+] Copiando Librerías Nvidia..."
+	cp /usr/lib/x86_64-linux-gnu/libGLX.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libGLdispatch.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-glcore.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-tls.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-glsi.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-gpucomp.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libGL.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libGLX_nvidia.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-glvkspirv.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-opticalflow.so* $DEST_DIR
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-rtcore.so* $DEST_DIR
+	
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-ngx.so* $DEST_DIR
+	ln -s libnvidia-ngx.so.570.158.01 libnvidia-ngx.so
+	ln -s libnvidia-ngx.so.570.158.01 libnvidia-ngx.so1
+
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-ml.so.570.158.01 
+	ln -s libnvidia-ml.so.570.158.01 libnvidia-ml.so
+	ln -s libnvidia-ml.so.570.158.01 libnvidia-ml.so1
+
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-cfg.so.570.158.01
+	ln -s libnvidia-cfg.so.570.158.01 libnvidia-cfg.so
+	ln -s libnvidia-cfg.so.570.158.01 libnvidia-cfg.so1
+	
+	cp /usr/lib/x86_64-linux-gnu/libnvidia-encode.so.570.158.01
+	ln -s libnvidia-encode.so.570.158.01 libnvidia-encode.so
+	ln -s libnvidia-encode.so.570.158.01 libnvidia-encode.so1
+
+
+
+	# Crear archivo JSON de vendor para GLVND
+	echo "[+] Configura librería..."
+	tee $DEST_DIR/50_nvidia.json >/dev/null <<EOF
+	{
+		"file_format_version": "1.0.0",
+		"ICD": {
+			"library_path": "libGLX_nvidia.so.0"
+		}
+	}
+EOF
+
+	#Soporte Vulkan
+	echo "[+] Copiando Librerías Vulkan..."
+	cp /usr/lib/x86_64-linux-gnu/libvulkan.so.1 $DEST_DIR
+	cp /usr/share/vulkan/icd.d/nvidia_icd.json $DEST_DIR
+	cp /usr/share/vulkan/implicit_layer.d/nvidia_layers.json $DEST_DIR
+
+	echo "[+] Ajustando rutas internas en los JSON..."
+	sed -i 's#"library_path": "libvulkan.so.1"#"library_path": "/opt/flatpak-nvidia-libs/libvulkan.so.1"#' $DEST_DIR/nvidia_icd.json
+	sed -i 's#"library_path": "libvulkan.so.1"#"library_path": "/opt/flatpak-nvidia-libs/libvulkan.so.1"#' $DEST_DIR/nvidia_layers.json
+
+
+
+	#Dar permisos adecuados
+	chmod 644 $DEST_DIR/50_nvidia.json
+	chmod 644 $DEST_DIR/nvidia_icd.json
+	chmod 644 $DEST_DIR/nvidia_layers.json
+	chmod -R 755 $DEST_DIR
+
+
+	#Aplicar override en Flatpak
+	#flatpak override --user --show com.usebottles.bottles			Para Pruebas
+	flatpak override --user com.usebottles.bottles \
+	--env=__EGL_VENDOR_LIBRARY_FILENAMES=/opt/flatpak-nvidia-libs/50_nvidia.json \
+	--env=VK_ICD_FILENAMES=/opt/flatpak-nvidia-libs/nvidia_icd.json \
+	--env=VK_LAYER_PATH=/opt/flatpak-nvidia-libs \
+	--env=VK_LAYER_ENABLE=VK_LAYER_NVIDIA_overlay \
+	--env=LD_LIBRARY_PATH=/opt/flatpak-nvidia-libs:/app/lib \
+	--env=__NV_DLSS_LIBRARY_PATH=/opt/flatpak-nvidia-libs/libnvidia-ngx.so.1
+	#--env=__NV_PRIME_RENDER_OFFLOAD=1 \		#Solo si tienes laptop
+	--env=__GLX_VENDOR_LIBRARY_NAME=nvidia \
+	--filesystem=/opt/flatpak-nvidia-libs:ro
+
+
+}
+
 echo "Instalando Flatpak ..."
 install_flatpak
+#echo "Configurando Nvidia with Flatpak ..."
+#install_flatpak_nvidia
 echo "Enjoy 3:)"
---
+++ install_java7.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Only Ubuntu
+function update(){
+	apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
+}
+
+update > /dev/null
+#if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
+
+function install_maven(){
+    apt install maven -y > /dev/null
+}
+
+function install_java7(){
+    #Java7
+    mkdir -p /usr/lib/jvm
+    tar -xzf src/jdk-7u80-linux-x64.tar.gz
+    mv jdk1.7.0_80/ /usr/lib/jvm/
+
+    update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0_80/bin/java" 1001
+    update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_80/bin/javac" 1001
+}
+
+function install_java8(){
+    #OpenJDK 8.0 o superiores
+    add-apt-repository ppa:openjdk-r/ppa -y > /dev/null
+    apt-get install openjdk-8-jdk -y
+}
+
+function install_tomcat(){
+    echo "---> Instalando TomCat 5.5.17  ... "
+    tar -xf src/apache-tomcat-5.5.17.tar.xz
+    mv apache-tomcat-5.5.17/ /opt/tomcat5
+}
+
+function install_netbeans(){
+	echo "---> Descargando Instalador ... "
+	#wget -q https://github.com/Friends-of-Apache-NetBeans/netbeans-installers/releases/download/v26-build1/apache-netbeans_26-1_amd64.deb
+    echo "---> Instalando NetBeans  ... "
+	dpkg -i src/apache-netbeans_26-1_amd64.deb > /dev/null
+}
+
+#echo "Elige Versión de Java  ... "
+#update-alternatives --config java
+
+echo "Instalando Maven"
+install_maven
+echo "Instalando Java 1.7 ..."
+install_java7
+echo "Instalando Java 1.8 ..."
+install_java8
+echo "Instalando TomCat 5.5.17 ..."
+install_tomcat
+echo "Instalando NetBeans ..."
+install_netbeans
+
+echo "Permisos"
+sudo chmod -R 755 /usr/lib/jvm/jdk1.7.0_80 /opt/tomcat5
+
+
+echo "Enjoy 3:)"
