c6f594aec28a7582299c6ef12d96c99c052a1c36
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-08-02 16:31:14 -0400

    [ADD] mejoras en modo root

--- install_xanmod.sh
+++ install_xanmod.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "Por favor, ejecute como root."
+    exit 1
+fi
+
 function update(){
 	apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
 }
@@ -6,7 +11,7 @@ function update(){
 source_list=/etc/apt/sources.list.d
 gpgkey_path=/etc/apt/trusted.gpg.d
 
-#update > /dev/null
+update > /dev/null
 if [ $(arch) == 'x86_64' ]; then archtype=[arch=amd64]; fi
 
 function install_xanmod(){
