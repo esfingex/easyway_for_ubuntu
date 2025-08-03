5539d911d58bfbc838241dfae968cf5e2ced7480
Iván Masías <ivan.masias.ortiz@gmail.com>
2025-08-03 01:02:01 -0400

    [ADD] virgl_support

--- install_kvm_qemu.sh
+++ install_kvm_qemu.sh
@@ -7,6 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 function update(){
+    echo "Actualizando el sistema..."
 	apt-get update -y && apt-get upgrade -y && apt-get autoremove -y
 }
 
@@ -22,13 +23,42 @@ function install_microcode(){
     fi
 }
 
+function install_virgl_support(){
+  echo "---> Instalando soporte adicional VirGL y SPICE..."
+  apt-get install -y \
+    spice-client-glib-usb-acl-helper gir1.2-spiceclientgtk-3.0 \
+    libspice-server1 libvirt-daemon-driver-video \
+    libosinfo-bin spice-client-gtk virglrenderer \
+    libepoxy0 libdrm-dev libegl1-mesa-dev \
+    libgles2-mesa-dev libwayland-client0 libxkbcommon0 \
+    libgbm1 libspice-client-glib-2.0-8 libvirglrenderer1 \
+    libvirglrenderer-dev spice-vdagent \
+    libgbm-dev libspice-protocol-dev libspice-server-dev \
+    libspice-client-gtk-3.0-5 > /dev/null
+
+    echo "---> Cargando Modulo Virtio Aceleracion 3D..."
+    modprobe virtio_gpu
+
+}
+
+
+#Instalacion standar falta la que corra vulkan con las VM
 function install_kvm_qemu(){
 
-    echo "---> Instalando KVM & Qemu ... "
-    apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+    if echo $XDG_SESSION_TYPE | grep -q wayland; then
+        echo "⚠️ Estás en Wayland. VirGL puede no funcionar correctamente. Usa X11 para mejor compatibilidad."
+    fi
 
-    echo "---> Agregando usuario actual a grupo libvirt..."
+    echo "---> Instalando KVM & Qemu ... "
+    apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients \
+        bridge-utils virt-manager mesa-utils qemu-system-x86 \
+        mesa-vulkan-drivers virt-viewer ovmf > /dev/null
+
+    echo "---> Agregando usuario actual a grupo libvirt ..."
     usermod -aG libvirt $(whoami)
+
+    echo "---> Agregando usuario libvirt al grupo de video..."
+    usermod -aG render,video,kvm libvirt-qemu
 
     echo "---> Habilitando y levantando libvirtd..."
     systemctl enable --now libvirtd
@@ -49,5 +79,6 @@ function install_kvm_qemu(){
 echo "Instalando kvm & qemu ..."
 install_microcode
 install_kvm_qemu
+install_virgl_support
 echo "Recuerda Reiniciar el Sistema ..."
 echo "Enjoy 3:)"
